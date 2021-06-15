91e3372e83e75052489d3f853f79f644b7cbd9da
amanimavu <amanimavu@gmail.com>
2021-06-15 20:34:12 +0300

    second commit

--- config/database.js
+++ config/database.js
@@ -4,16 +4,17 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
+        //host: env('DATABASE_HOST', '127.0.0.1'),
+        uri: env('DATABASE_URI', 'mongodb+srv://butika:butika@butikabackend.ef4cz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
+        srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'my-project'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        //username: env('DATABASE_USERNAME', null),
+        //password: env('DATABASE_PASSWORD', null),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
